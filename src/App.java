0221f0f5004427294c42af10814a70bfa4f84c67
alejacorrea <alejandracp63@gmail.com>
2025-04-03 18:17:07 -0500

    Se empezo a nombrar las variables y desarrollar el ejercicio

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+    public class App {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner (System.in);
+            String nombreEmpleado, documentoEmpleado, diaDescanso ="";
+            double salarioBruto = 0;
+            double salarioNeto = 0;
+            double deduccionPension = 0;
+            double deduccionSalud = 0;
+            double horasTrabajadas = 0;
+            double valorHora = 0;
+            double bonificacionEmpleado = 0;
+            double valorSalarioMinimo = 0;
+            double auxilioTransporte = 0;
+            double cantidadHorasExtras = 0;
+            double valorHoraExtra = 0;
+            double totalValorHorasExtras = 0;
+
+            System.out.println("Ingrese el nombre del empleado: ");
+            nombreEmpleado = sc.nextLine();
+            System.out.println("Ingrese el documento del empleado: ");
+            documentoEmpleado = sc.nextLine();
+            System.out.println("Ingrese el dia de descanso: (Lunes-Viernes)");
+            diaDescanso = sc.nextLine();
+            
+        }
+
     }
-}
+    
