2bc3c88e327b5f2c185b7abc7665076526e5e610
alejacorrea <alejandracp63@gmail.com>
2025-04-10 16:23:21 -0500

    Ejercicio finalizado

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Se debe ingresar numero de documento de identidad
 El empleado podra seleccionar un dia de descanso de Lunes a Viernes
 El empleado tendra auxilio de transporte si su salario bruto no supera los 2 SMLV
 El empleado recibira una bonificacion del 10% sobre su salario bruto si no supera los 2 SMLV
-El empleado no podra trabajar menos de 12 horas
+El empleado no podra trabajar menos de 24 horas
 El empleado recibira pago de horas extras si supera las 96 horas
 Calcular las deducciones del salud y pensiones
 El sistema debe mostrar toda la informacion del empleado de manera detallada
--- src/App.java
+++ src/App.java
@@ -1,30 +1,72 @@
 import java.util.Scanner;
 
-    public class App {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner (System.in);
-            String nombreEmpleado, documentoEmpleado, diaDescanso ="";
-            double salarioBruto = 0;
-            double salarioNeto = 0;
-            double deduccionPension = 0;
-            double deduccionSalud = 0;
-            double horasTrabajadas = 0;
-            double valorHora = 0;
-            double bonificacionEmpleado = 0;
-            double valorSalarioMinimo = 0;
-            double auxilioTransporte = 0;
-            double cantidadHorasExtras = 0;
-            double valorHoraExtra = 0;
-            double totalValorHorasExtras = 0;
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nombreEmpleado, documentoEmpleado, diaDescanso = "";
+        double salarioBruto = 0;
+        double salarioNeto = 0;
+        double deduccionPension = 0;
+        double deduccionSalud = 0;
+        double horasTrabajadas = 0;
+        double valorHora = 0;
+        double bonificacionEmpleado = 0;
+        double valorSalarioMinimo = 1423500;
+        double auxilioTransporte = 200000;
+        double cantidadHorasExtras = 0;
+        double valorHoraExtra = 0;
+        double totalValorHorasExtras = 0;
 
-            System.out.println("Ingrese el nombre del empleado: ");
-            nombreEmpleado = sc.nextLine();
-            System.out.println("Ingrese el documento del empleado: ");
-            documentoEmpleado = sc.nextLine();
-            System.out.println("Ingrese el dia de descanso: (Lunes-Viernes)");
-            diaDescanso = sc.nextLine();
-            
+        System.out.println("Ingrese el nombre del empleado: ");
+        nombreEmpleado = sc.nextLine();
+        System.out.println("Ingrese el documento del empleado: ");
+        documentoEmpleado = sc.nextLine();
+        System.out.println("Ingrese el dia de descanso: (Lunes-Viernes)");
+        diaDescanso = sc.nextLine();
+        System.out.println("Ingrese el valor de la hora: ");
+        valorHora = sc.nextDouble();
+        System.out.println("Ingrese la cantidad de horas trabajadas: ");
+        horasTrabajadas = sc.nextDouble();
+
+        salarioBruto = horasTrabajadas * valorHora;
+        if (salarioBruto <= valorSalarioMinimo * 2) {
+            auxilioTransporte = 200000;
+            bonificacionEmpleado = salarioBruto * 0.1;
+        } else {
+            auxilioTransporte = 0;
+            bonificacionEmpleado = 0;
         }
 
+        deduccionPension = salarioBruto * 0.04;
+        deduccionSalud = salarioBruto * 0.04;
+
+        if (horasTrabajadas > 96) {
+            cantidadHorasExtras = horasTrabajadas - 96;
+            valorHoraExtra = valorHora * 1.25;
+            totalValorHorasExtras = cantidadHorasExtras * valorHoraExtra;
+        }
+
+        if (horasTrabajadas < 24) {
+            System.out.println("Aun no le puedo pagar, trabaje más");
+        } else {
+            salarioNeto = salarioBruto - deduccionPension - deduccionSalud + auxilioTransporte + bonificacionEmpleado
+                    + totalValorHorasExtras;
+            System.out.println("El salario neto del empleado es: " + salarioNeto);
+        }
+
+        System.out.println("El nombre del empleado es:" + nombreEmpleado);
+        System.out.println("El documento del empleado es: " + documentoEmpleado);
+        System.out.println("El dia de descanso será: " + diaDescanso);
+        System.out.println("El salario bruto es de: " + salarioBruto);
+        System.out.println("El valor de la hora trabajada es de: " + valorHora);
+        System.out.println("La cantidad de horas trabajadas es de: " + horasTrabajadas);
+        System.out.println("El valor del salario neto es: " + salarioNeto);
+        System.out.println("La bonificacion del empleado es de: " + bonificacionEmpleado);
+        System.out.println("El auxilio de transporte es de: " + auxilioTransporte);
+        System.out.println("Las deducciones de pension es de: " + deduccionPension);
+        System.out.println("Las deducciones de pension es de: " + deduccionSalud);
+        System.out.println("La cantidad de horas extras son: " + cantidadHorasExtras);
+        System.out.println("El total de las horas extras trabajadas son: " + totalValorHorasExtras);
     }
-    
+
+}
